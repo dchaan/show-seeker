e81cbcd0a8c002e7a44e753921337146380fe4e7
David Chan <dchaan1@gmail.com>
2023-08-31 11:48:44 -0500

    migration script

--- app/api/genre_routes.py
+++ app/api/genre_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify
 from app.ticketmaster_api import get_genres_from_classifications, get_classifications_from_api, get_genre_by_id_from_classifications
 
 genre_routes = Blueprint('genres', __name__)
