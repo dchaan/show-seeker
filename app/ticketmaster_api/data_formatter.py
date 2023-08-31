d6a5e1dd0ba7940fd5549d205e1f34ffa40d8147
David Chan <dchaan1@gmail.com>
2023-08-31 11:16:39 -0500

    refactor format classification, remove ids from format methods

--- app/ticketmaster_api/data_formatter.py
+++ app/ticketmaster_api/data_formatter.py
@@ -1,11 +1,12 @@
 def format_event(event):
-  artist = event['_embedded']['attractions'][0]
+  attractions = event['_embedded'].get('attractions', [])
+  artist = attractions[0] if attractions else None
+
   venue = event['_embedded']['venues'][0]
   classification = event['classifications'][0]
   genre = classification['genre']
   
   event_data = {
-    'id': event['id'],
     'name': event['name'],
     'start_time': event['dates']['start'].get('dateTime', None),
     'promoter': event.get('promoter', {}).get('description', None),
@@ -15,22 +16,19 @@ def format_event(event):
     'ticket_limit': event.get('ticketLimit', {}).get('info', None),
     'url': event['url'],
     'images': [image['url'] for image in event.get('images', [])],
-    'artist': artist['id'],
+    'artist': artist['id'] if artist else None,
     'venue': venue['id'],
     'classification': classification['segment']['id'],
     'genre': genre['id']
   }
-  
   return event_data
 
 def format_artist(artist):
   artist_data = {
-    'id': artist['id'],
     'name': artist['name'],
     'genre_id': artist['classifications'][0]['genre']['id'],
     'external_links': []
   }
-
   links = artist.get('externalLinks', {})
 
   for platform, link_list in links.items():
@@ -40,30 +38,18 @@ def format_artist(artist):
           'name': platform.capitalize(),
           'url': link['url'],
         })
-  
   return artist_data
 
 def format_classification(classification):
+  segment_data = classification.get("segment", {})
   classification_data = {
-    "id": None,
-    "name": None,
-    "genres": []
+    "id": segment_data.get("id"),
+    "name": segment_data.get("name"),
+    "genres": [
+      {"id": genre.get("id"), "name": genre.get("name")}
+      for genre in segment_data.get("_embedded", {}).get("genres", [])
+    ]
   }
-
-  if "segment" in classification:
-    segment_data = classification["segment"]
-    classification_data["id"] = segment_data.get("id")
-    classification_data["name"] = segment_data.get("name")
-
-    if "_embedded" in segment_data and "genres" in segment_data["_embedded"]:
-      genres = segment_data["_embedded"]["genres"]
-      for genre_data in genres:
-        genre = {
-          "id": genre_data.get("id"),
-          "name": genre_data.get("name")
-        }
-        classification_data["genres"].append(genre)
-
   return classification_data
 
 def format_venue(venue, events):
@@ -76,7 +62,6 @@ def format_venue(venue, events):
   ]
 
   venue_data = {
-    'id': venue['id'],
     'name': venue['name'],
     'address': ', '.join(filter(None, address)),
     'box_office_info': venue['boxOfficeInfo']['openHoursDetail'] if 'boxOfficeInfo' in venue and 'openHoursDetail' in venue['boxOfficeInfo'] else '',
@@ -84,5 +69,4 @@ def format_venue(venue, events):
     'images': [image['url'] for image in venue['images']] if 'images' in venue else [],
     'events': events
   }
-  
   return venue_data
