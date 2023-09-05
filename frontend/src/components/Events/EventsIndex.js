85fb7eb7d0978c5ba0e915031d5832d96562add3
David Chan <dchaan1@gmail.com>
2023-09-05 13:27:32 -0500

    add pagination

--- frontend/src/components/Events/EventsIndex.js
+++ frontend/src/components/Events/EventsIndex.js
@@ -1,5 +1,6 @@
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
 import styles from "./Events.module.css";
 import { getEvents } from "../../store/event";
 import EventCard from "./EventCard";
@@ -7,6 +8,8 @@ import EventCard from "./EventCard";
 const EventsIndex = () => {
   const events = useSelector(state => state.event.events);
   const dispatch = useDispatch();
+  const [displayedEvents, setDisplayedEvents] = useState(10);
+  const batchSize = 10;
 
   useEffect(() => {
     dispatch(getEvents());
@@ -14,9 +17,42 @@ const EventsIndex = () => {
 
   return (
     <div>
-      {events.map(event => (
-        <EventCard event={event} />
-      ))}
+      <div className={styles.subHeader}>
+        <div className={styles.subHeaderContainer}>
+          <div className={styles.homeContainer}>
+              <div className={styles.homeTextContainer}>
+                <NavLink className={styles.homeLink} to="/">Home</NavLink>
+                <div className={styles.slash}>
+                  /
+                </div>
+                <div className={styles.subTitle}>Concert Tickets</div>
+              </div>
+          </div>
+          <div className={styles.subHeaderTitle}>
+            <h1 className={styles.subHeaderTitleText}>
+              <span className={styles.concertText}>Concert </span>
+              <span className={styles.ticketsText}>Tickets</span>
+            </h1>
+          </div>
+        </div>
+      </div>
+      <div className={styles.eventsContainer}>
+        <div className={styles.eventsTitleContainer}>
+          <div className={styles.allConcertsText}>All Concert Events ({events.length})</div>
+        </div>
+        {events.slice(0, displayedEvents).map(event => (
+          <EventCard event={event} />
+        ))}
+      </div>
+      <div className={styles.loadMoreContainer}>
+        {displayedEvents < events.length && (
+          <div className={styles.loadMoreSubContainer}>
+            <button className={styles.loadMoreButton} onClick={() => setDisplayedEvents(prev => prev + batchSize)}>
+              Load More
+            </button>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
