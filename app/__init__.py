172fd8adae2fdc9ad473d66369b75f4439448a11
David Chan <dchaan1@gmail.com>
2023-09-27 10:39:00 -0500

    Add to index.js and init.py

--- app/__init__.py
+++ app/__init__.py
@@ -46,10 +46,28 @@ def inject_csrf_token(response):
     httponly=True)
   return response
 
+@app.route("/api/docs")
+def api_help():
+  """
+  Returns all API routes and their doc strings
+  """
+  acceptable_methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+  route_list = {rule.rule: [
+    [method for method in rule.methods if method in acceptable_methods],
+    app.view_functions[rule.endpoint].__doc__]
+    for rule in app.url_map.iter_rules() if rule.endpoint != 'static'}
+  return route_list
+
+
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def react_root(path):
+  """
+  This route will direct to the public directory in our
+  react builds in the production environment for favicon
+  or index.html requests
+  """
   if path == 'favicon.ico':
-    return app.send_static_file('favicon.ico')
+    return app.send_from_directory('public', 'favicon.ico')
   return app.send_static_file('index.html')
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,8 +4,28 @@ import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import configureStore from './store';
+import * as artistActions from "./store/artist";
+import * as classificationActions from "./store/classification";
+import * as eventActions from "./store/event";
+import * as favoriteActions from "./store/favorites";
+import * as genreActions from "./store/genre";
+import * as purchaseActions from "./store/purchases";
+import * as sessionActions from "./store/session";
+import * as venueActions from "./store/venue";
 
 const store = configureStore();
+
+if (process.env.NODE_ENV !== "production") {
+  window.store = store;
+  window.artistActions = artistActions;
+  window.classificationActions = classificationActions;
+  window.eventActions = eventActions;
+  window.favoriteActions = favoriteActions;
+  window.genreActions = genreActions;
+  window.purchaseActions = purchaseActions;
+  window.sessionActions = sessionActions;
+  window.venueActions = venueActions;
+}
 
 ReactDOM.render(
   <React.StrictMode>
