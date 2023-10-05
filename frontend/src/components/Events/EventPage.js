42ecd7e0bb2a58f65e79684031921520bd02be81
David Chan <dchaan1@gmail.com>
2023-10-05 00:03:44 -0500

    remove consoles and change fetch #

--- frontend/src/components/Events/EventPage.js
+++ frontend/src/components/Events/EventPage.js
@@ -25,9 +25,6 @@ const EventPage = () => {
 
   if (!isLoaded) return <div>Loading...</div>;
 
-  console.log(event.artist_api_id)
-  console.log(event)
-
   const openImportantInfoModal = () => {
     setImportantInfoModal(true);
   };
