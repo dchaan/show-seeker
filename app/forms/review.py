a6da7c64e9c4499bcf94b935461ebd30d25d583f
David Chan <dchaan1@gmail.com>
2023-10-05 14:30:36 -0500

    fix form error

--- app/forms/review.py
+++ app/forms/review.py
@@ -5,7 +5,7 @@ from wtforms.validators import DataRequired, Length
 class ReviewForm(FlaskForm):
   rating = IntegerField('rating', validator=[DataRequired('must submit rating')])
 
-  title = StringField('title', validators=[DataRequired('must submit a title', Length(1, 100))])
+  title = StringField('title', validators=[DataRequired('must submit a title'), Length(1, 100)])
 
   body = StringField('body', validators=[DataRequired('must submit a body'), Length(1, 1000)])
 
