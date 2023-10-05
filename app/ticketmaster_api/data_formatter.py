6c5c77dae2c30b6b9a4f73b02024a82f7d5e0210
David Chan <dchaan1@gmail.com>
2023-10-04 23:22:58 -0500

    u

--- app/ticketmaster_api/data_formatter.py
+++ app/ticketmaster_api/data_formatter.py
@@ -37,8 +37,8 @@ def format_event(event):
     'venue_api_id': venue_api_id,
     'classification_api_id': classification_api_id,
     'genre_api_id': genre_api_id,
-    'artist_api': artist,
-    'venue_api': venue
+    'artist': artist,
+    'venue': venue
   }
   return event_data
 
