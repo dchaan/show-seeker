6d885ce6f58a0db0f889a6bbe4ae80ac7db0ad31
David Chan <dchaan1@gmail.com>
2023-10-11 11:57:50 -0500

    update events routes

--- app/api/event_routes.py
+++ app/api/event_routes.py
@@ -2,7 +2,6 @@ from flask import Blueprint, jsonify, request
 from app.models import Event, Venue, Classification, Genre, Artist
 from sqlalchemy import or_
 
-
 event_routes = Blueprint('events', __name__)
 
 @event_routes.route('/', methods=['GET'])
@@ -10,21 +9,15 @@ def get_events():
   query = request.args.get('query')
 
   if query:
-    events = Event.query \
-      .join(Artist, Event.artist) \
-      .join(Venue, Event.venue) \
-      .join(Genre, Event.genre) \
-      .join(Classification, Event.classification) \
-      .filter(
-        or_(
-          Event.name.ilike(f'%{query}%'),
-          Artist.name.ilike(f'%{query}%'),
-          Venue.name.ilike(f'%{query}%'),
-          Genre.name.ilike(f'%{query}%'),
-          Classification.name.ilike(f'%{query}%')
-        )
-      ) \
-      .all()
+    events = Event.query.filter(
+      or_(
+        Event.name.ilike(f'%{query}%'),
+        Event.artist.has(Artist.name.ilike(f'%{query}%')),
+        Event.venue.has(Venue.name.ilike(f'%{query}%')),
+        Event.genre.has(Genre.name.ilike(f'%{query}%')),
+        Event.classification.has(Classification.name.ilike(f'%{query}%'))
+      )
+    ).all()
   else:
     events = Event.query.all()
 
